d766e6eb340d6a031cc65c2b253078e0a8762270
BBernYY <screitsma@proton.me>
2025-01-30 21:33:19 +0100

    improved sliders and added support for dragging objects

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+from math import log, ceil
 
 pygame.init()
 
@@ -41,6 +42,9 @@ class Vec2:
         if mag == 0:
             return Vec2(0,0)
         return (1/mag) * vector
+def from_absolute(xy):
+    x, y = xy
+    return Vec2(x - SCREEN_WIDTH*0.5, 0.5*SCREEN_HEIGHT-y)
 
 
 class Slider():
@@ -48,47 +52,66 @@ class Slider():
     maxval: float
     step: float
     val: float
-    font = pygame.font.Font(None, 74)
+    label: str
+    font = pygame.font.Font(None, 32)
 
 
-    def __init__(self, rang, step, keybind, center, val=None):
+    def __init__(self, label, rang, step, keybind, center, val=None):
+        self.label = label
         self.minval, self.maxval = rang
         self.step = step
         self.increase, self.decrease = keybind
         self.center = center
+    
         if not val:
             self.val = self.minval + 0.5*(self.maxval - self.minval)
+    def __repr__(self):
+        decimals = ceil(-log(self.step))
+        if decimals > 0:
+            s = str(round(self.val, decimals))
+        else:
+            s = str(int(self.val))
+        return self.label+": "+s
     def draw(self):
-        self.surface = self.font.render(str(self.val), True, WHITE)
+        self.surface = self.font.render(str(self), True, WHITE)
         self.rect = self.surface.get_rect(center=self.center.get_absolute())
         screen.blit(self.surface, self.rect)
 
 
     def check_binds(self):
         keys = pygame.key.get_pressed()
-        if keys[self.increase] and self.val + self.step < self.maxval:
+        if keys[self.increase] and self.val + self.step <= self.maxval:
             self.val += self.step
-        elif keys[self.decrease] and self.val - self.step > self.minval:
+        elif keys[self.decrease] and self.val - self.step >= self.minval:
             self.val -= self.step
-grav = Slider((-100, 100), 1, (pygame.K_UP, pygame.K_DOWN), Vec2(0, 0))
+grav = Slider("G", (-100, 100), 1, (pygame.K_UP, pygame.K_DOWN), Vec2(-300, -250))
 
-timespeed = Slider((-10, 10), 0.1, (pygame.K_i, pygame.K_k), Vec2(0, 100))
-
+timespeed = Slider("t", (-100, 100), 0.1, (pygame.K_i, pygame.K_k), Vec2(-200, -250))
+timestop = False
 class Body:
+    drag = False
     def gravitay(self, other):
         r = (other.pos + -1*self.pos).magnitude()
+        #if r < self.radius + other.radius:
+        #    self.vel = -1 * self.vel
+        #    return 
         mag = grav.val*self.mass*other.mass / r**2
         self.netF += mag * self.pos.get_pointing_unitvector(other.pos)
         
 
     def movement_update(self):
+        if self.drag:
+            keys = pygame.mouse.get_pressed()
+            if keys[0]:
+                self.pos = from_absolute(pygame.mouse.get_pos())
+                return
+        self.drag = False
         self.acc = 1/self.mass * self.netF
         self.netF = Vec2(0,0)
 
-        self.vel += (timespeed.val*(1/60) * self.acc)
+        self.vel += (timespeed.val*int(timestop)*(1/60) * self.acc)
 
-        self.pos += (timespeed.val*(1/60) * self.vel)
-        
+        self.pos += (timespeed.val*int(timestop)*(1/60) * self.vel)
 
 
     def draw(self):
@@ -104,16 +127,28 @@ class Body:
 
 # Main game loop
 def main():
+    global timestop
     running = True
 
     earth = Body(color=(0, 0, 255), pos=Vec2(0, 0), radius=63, mass=50)
-    moon = Body(color=WHITE, pos=Vec2(38, 0), radius=17, mass=74, vel=Vec2(0,8))
-    boon = Body(color=(0, 255, 0), pos=Vec2(-12, 0), radius=27, mass=63, vel=Vec2(0,-3))
+    moon = Body(color=WHITE, pos=Vec2(38, 0), radius=17, mass=74)
+    boon = Body(color=(0, 255, 0), pos=Vec2(-12, 0), radius=27, mass=63)
+    bodies = [earth, moon, boon]
+
     while running:
         # Event handling
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_SPACE:
+                    timestop = not timestop
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mousepos = from_absolute(event.pos)
+                for body in bodies:
+                    dist = (body.pos + -1 * mousepos).magnitude()
+                    if dist < body.radius:
+                        body.drag = True
         grav.check_binds()
         timespeed.check_binds()
 
